860ced1f98d28208f3a3c07857e437da286fcdfc
ojt.huy.le <ojt.huy.le@napaglobal.com>
2025-08-20 10:32:32 +0700

    add MongoDBSaver to save memory chat: in progess

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ from nodes.fallback import fallback_node
 from nodes.planner import planner_node
 from nodes.finalizer import finalizer_node
 from langchain_core.messages import HumanMessage
-from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.mongodb import MongoDBSaver
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-# Mở rộng MessagesState để lưu decision từ supervisor
 class ExtendedState(MessagesState):
-    decision: str = ""  # Lưu quyết định của supervisor
+    decision: str = ""  # save decision of supervisor
 
 # Build graph
 graph_builder = StateGraph(ExtendedState)
-
-# Thêm nodes
 graph_builder.add_node("planner", planner_node)
 graph_builder.add_node("supervisor", supervisor_node)
 graph_builder.add_node("fallback", fallback_node)
@@ -22,9 +23,9 @@ graph_builder.add_node("finalizer", finalizer_node)
 # Edges 
 graph_builder.add_edge(START, "planner")
 
-# Conditional edges từ Planner
+# Conditional edges from Planner
 def route_from_planner(state: ExtendedState):
-    return state["decision"]  # "supervisor" hoặc "fallback"
+    return state["decision"]  # "supervisor" or "fallback"
 
 graph_builder.add_conditional_edges(
     "planner",
@@ -36,12 +37,23 @@ graph_builder.add_edge("supervisor", "finalizer")
 graph_builder.add_edge("finalizer", END)
 graph_builder.add_edge("fallback", END)
 
-# memory = MemorySaver()
+# DB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017/history_chatbot")
+# try:
+#     client = MongoClient(DB_URI)
+#     client.admin.command('ping')
+#     print("Connect mongo successfully!")
+# except Exception as e:
+#     print(f"Errors in MongoDB: {e}")
+#     exit(1)
 
+
+# with MongoDBSaver.from_conn_string(DB_URI) as checkpointer:
 graph = graph_builder.compile()
 
-
 if __name__ == "__main__":
+
+    config = {"configurable": {"thread_id": "lvqh-43"}}
+
     while True:
         user_input = input("You: ")
 
@@ -54,11 +66,8 @@ if __name__ == "__main__":
         input_message = HumanMessage(content=user_input)
         
         try:
-            # use config to use memorysaver
-            # config = {"configurable": {"thread_id": "user-43"}}
             result = graph.invoke({"messages": [input_message]})
-            print("Final answer: ")
-            print(result["messages"][-1].content)
+            print("Final answer:", result["messages"][-1].content)
             
         except Exception as e:
             print(f"Errors: {e}")

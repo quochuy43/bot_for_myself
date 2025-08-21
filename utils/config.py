b933869586812c692c39df5a1879f1421462aab4
ojt.huy.le <ojt.huy.le@napaglobal.com>
2025-08-21 13:34:10 +0700

    create rag folder, knowledge_base for personal information

--- utils/config.py
+++ utils/config.py
@@ -1,14 +1,15 @@
 import os
 # from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_openai import ChatOpenAI
-from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain_openai import ChatOpenAI
+# from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
 load_dotenv()
 
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
-MODEL_NAME = "gpt-4o-mini"
-model = ChatOpenAI(model=MODEL_NAME)
+MODEL_NAME = "openai:gpt-4o-mini"
+model=init_chat_model(MODEL_NAME, temperature=0.3)
 
 
 # os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
